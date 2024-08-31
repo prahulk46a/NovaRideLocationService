076a01b600549824d7e02dc9ca2d6313c17da340
Rahul Patil <[prahulk212@gmail.com]>
2024-08-31 17:09:24 +0530

    Added eureka client

--- src/main/java/org/novaride/novaridelocationservice/NovaRideLocationServiceApplication.java
+++ src/main/java/org/novaride/novaridelocationservice/NovaRideLocationServiceApplication.java
@@ -2,8 +2,10 @@ package org.novaride.novaridelocationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NovaRideLocationServiceApplication {
 
     public static void main(String[] args) {
